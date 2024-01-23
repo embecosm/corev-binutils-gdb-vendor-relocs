f39caad814ef97c9b0d256249e2a1fcccfa9146f
Pietra Ferreira <pietra.ferreira@embecosm.com>
2024-01-23 13:59:28 +0000

    Add gas tests for Core-V vendor relocs

---
+++ gas/testsuite/gas/riscv/cv-hwlp-setupi-urs1.d
@@ -0,0 +1,24 @@
+#source: cv-hwlp-setupi-urs1.s
+#as: -march=rv32ixcvhwlp -mabi=ilp32
+#objdump: -dr
+
+.*:[    ]+file format .*
+
+
+Disassembly of section .text:
+
+.* <func>:
+[ 	]+0:[ 	]+00008067[ 	]+ret
+
+.* <_start>:
+[ 	]+4:[ 	]+00a0462b[ 	]+cv.setupi[ 	]+0,10,4 <_start>
+[ 	]+4:[ 	]+R_RISCV_RELOCID[ 	]+\*ABS\*\+0x6376
+[ 	]+4:[ 	]+R_RISCV_CVPCREL_URS1[ 	]+L2
+[ 	]+8:[ 	]+00000097[ 	]+auipc[ 	]+ra,0x0
+[ 	]+8:[ 	]+R_RISCV_CALL_PLT.*
+[ 	]+8:[ 	]+R_RISCV_RELAX.*
+[ 	]+c:[ 	]+000080e7[ 	]+jalr[ 	]+ra.*
+
+.* <L2>:
+[ 	]+10:[ 	]+00000013[ 	]+nop
+#pass
---
+++ gas/testsuite/gas/riscv/cv-hwlp-starti-ui12.d
@@ -0,0 +1,24 @@
+#source: cv-hwlp-starti-ui12.s
+#as: -march=rv32ixcvhwlp -mabi=ilp32
+#objdump: -dr
+
+.*:[    ]+file format .*
+
+
+Disassembly of section .text:
+
+.* <func>:
+[ 	]+0:[ 	]+00008067[ 	]+ret
+
+.* <_start>:
+[ 	]+4:[ 	]+0000402b[ 	]+cv.starti[ 	]+0,4 <_start>
+[ 	]+4:[ 	]+R_RISCV_RELOCID[ 	]+\*ABS\*\+0x6376
+[ 	]+4:[ 	]+R_RISCV_CVPCREL_UI12[ 	]+L2
+[ 	]+8:[ 	]+00000097[ 	]+auipc[ 	]+ra,0x0
+[ 	]+8:[ 	]+R_RISCV_CALL_PLT.*
+[ 	]+8:[ 	]+R_RISCV_RELAX.*
+[ 	]+c:[ 	]+000080e7[ 	]+jalr[ 	]+ra.*
+
+.* <L2>:
+[ 	]+10:[ 	]+00000013[ 	]+nop
+#pass
